ee547f586dee35bcbcb6eef99d02406bda788c9d
MAGOSMASON <magosmason@gmail.com>
2026-01-21 10:53:07 -0600

    Initial commit

---
+++ CORNY/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CORNY.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
